76d973a18189055f3b99e108b109f94ebe4a89f2
MarcosFalcon343 <marcosfalcon2003@gmail.com>
2023-04-14 01:09:51 -0600

    update

---
+++ ProyectoIntegrador/Vistas/Principal/Principal.java
@@ -0,0 +1,32 @@
+package Principal;
+
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTree;
+import javax.swing.border.EmptyBorder;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class Principal extends JFrame {
+
+
+	public Principal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setBounds(100, 100, 450, 300);
+
+		
+	}
+
+}
---
+++ ProyectoIntegrador/src/Hola.java
@@ -0,0 +1,11 @@
+import Principal.Principal;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Principal principal = new Principal();
+		principal.setVisible(true);
+	}
+
+}
